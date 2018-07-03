47f6e765ea143df8d24777f1fb7701dd5378a7e9
EC2 Default User <ec2-user@ip-172-31-10-83.ap-southeast-1.compute.internal>
2018-07-03 10:41:59 +0000

    added usage guide

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-/* PRODUCER */
+/* DEPENDENCIES */
+
 const kafka = require('kafka-node');
 const argv = require('yargs').argv;
 
-const user_msg = argv.msg || '<default msg>';
+/* PROCESS INPUT ARGS */
 
+if (argv.help || argv.h) {
+    console.log(`USAGE: node producer-test [--msg="<your msg>"] [--topic=<kafka topic. defaults to 'test'>]`);
+    process.exit(0);
+}
+
+const user_msg = argv.msg || '<default msg>';
 const FULL_MSG = user_msg + ' -- sent via Node JS @' + Date.now();
 const TOPIC = argv.topic || 'test';
+console.log('TESTING W/ THE FF INPUTS:', {user_msg, TOPIC, FULL_MSG});
+
+/* SETUP */
 
 const Producer = kafka.Producer;
 // const KeyedMessage = kafka.KeyedMessage;
 
 const client = new kafka.Client();
 const producer = new Producer(client);
-
 // const km = new KeyedMessage('key', 'message');
 
 const payloads = [
@@ -20,8 +29,8 @@ const payloads = [
         // { topic: 'test', messages: ['NODE hello', 'world', km] }
     ];
     
-console.log('TESTING W/ THE FF INPUTS:', {user_msg, TOPIC, FULL_MSG});
-    
+/* SEND MSG TO KAFKA, SETUP HANDLERS */
+
 producer.on('ready', () => {
     producer.send(payloads, (err, data) => {
         if (err) {
