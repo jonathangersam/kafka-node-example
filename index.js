2664ce0167fed3b2d5ed33642138fc003a20d2a6
EC2 Default User <ec2-user@ip-172-31-10-83.ap-southeast-1.compute.internal>
2018-07-03 10:29:24 +0000

    get msg from arg input

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 /* PRODUCER */
 const kafka = require('kafka-node');
+const argv = require('yargs').argv;
+
+const user_msg = argv.msg || '<default msg>';
+
+const FULL_MSG = user_msg + ' -- sent via Node JS @' + Date.now();
+const TOPIC = argv.topic || 'test';
 
 const Producer = kafka.Producer;
 // const KeyedMessage = kafka.KeyedMessage;
@@ -10,16 +16,24 @@ const producer = new Producer(client);
 // const km = new KeyedMessage('key', 'message');
 
 const payloads = [
-        { topic: 'test', messages: 'NODE hi', partition: 0 },
+        { topic: TOPIC, messages: FULL_MSG, partition: 0 },
         // { topic: 'test', messages: ['NODE hello', 'world', km] }
     ];
     
+console.log('TESTING W/ THE FF INPUTS:', {user_msg, TOPIC, FULL_MSG});
+    
 producer.on('ready', () => {
     producer.send(payloads, (err, data) => {
-        console.log(data, err);
+        if (err) {
+            console.log('FAILED', err);
+            process.exit(0);
+        } 
+        console.log('SUCCESS', data);
+        process.exit(0);
     });
 });
  
 producer.on('error', (err) => {
-    console.log(err);
+    console.log('FAILED', err);
+    process.exit(0);
 });
